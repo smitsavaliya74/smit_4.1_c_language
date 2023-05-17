fd69b2209583aeb4e5cde20a2367f6edbfc03463
RomilVasoya <romil.vasoya22@gmail.com>
2023-05-17 12:40:43 +0530

    4.1

---
+++ SWAP1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main ()
+{
+ int a=5;
+ int b=10;
+ int c;
+ clrscr();
+ c=b;
+ b=a;
+ a=c;
+ printf("value of a is: %d\n",a);
+ printf("value of b is: %d\n",b);
+ getch();
+
+
+
+
+
+}
---
+++ SWAP2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a;
+int b;
+clrscr();
+a=10;
+b=15;
+a=b;
+printf("value of a is : %d\n",a);
+
+b=a;
+printf("value of b is : %d",b);
+
+getch();
+}
